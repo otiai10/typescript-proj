6a942f6ffffcfb626c83251081ede346d5927975
KAZUMA Ukyo <yaakaito@gmail.com>
2013-05-08 18:28:28 +0900

    Update index.ts

--- test/index.ts
+++ test/index.ts
@@ -2,9 +2,8 @@
 /// <reference path="../d.ts/mocha.d.ts" />
 /// <reference path="../d.ts/chai.d.ts" />
 
-var expect = chai.expect;
-
 describe('index', () => {
+    var expect = chai.expect;
     it('has text `Hello TypeScript!`', () => {
         var index = new Index.HelloWorld();
         expect(index.text()).to.equal('Hello TypeScript!');
