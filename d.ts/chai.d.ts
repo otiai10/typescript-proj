86cb4fa5902dd2c6b4690ecbd4ae6c5fea249480
KAZUMA Ukyo <yaakaito@gmail.com>
2013-05-27 19:59:54 +0900

    Update chai.d.ts

--- d.ts/chai.d.ts
+++ d.ts/chai.d.ts
@@ -1,3 +1,23 @@
 declare module chai {
     export function expect(value: any): any;
+    
+    interface IChai {
+        to: IChai;
+        be: IChai;
+        been: IChai;
+        is: IChai;
+        and: IChai;
+        have: IChai;
+        with: IChai;
+        that: IChai;
+        at: IChai;
+        of: IChai;
+        same: IChai;
+        not: IChai;
+        deep: IChai;
+        equal: any;
+        equals: any;
+        eq: any;
+        eql: any;
+    }
 }
